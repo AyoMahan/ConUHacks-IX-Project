7e170f64b1c6438fea77fd30ad8fe4e983e827db
Jessey Thach <94651491+jesseythach@users.noreply.github.com>
2025-02-01 16:05:46 -0500

    Player and room menu components

---
+++ Crafted-Duels/src/lib/components/Player.svelte
@@ -0,0 +1,4 @@
+<div class="player">
+    <img src="/images/profile.jpg" alt="Player profile"/>
+    <p>{playerName}</p>
+</div>
---
+++ Crafted-Duels/src/lib/components/RoomMenu.svelte
@@ -0,0 +1,16 @@
+<script>
+    import Player from "./Player.svelte";
+
+</script>
+<div class=room-menu-component>
+    <div class="top-menu">
+        <input value={playerName}/>
+    </div>
+    <div class="bottom-menu">
+        {Player}
+        <div class="seperator">
+            vs
+        </div>
+        {Player}
+    </div>
+</div>
